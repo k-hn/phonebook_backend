c1447a8923664321a6721450103d00e9896adeaf
Hilary <125813796+k-hn@users.noreply.github.com>
2023-02-26 01:55:44 +0000

    add exercise 3.5 solution

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     "id": 1,
@@ -59,6 +61,44 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter(person => person.id !== id)
   // Send No content(204) to indicate success
   response.status(204).end()
+})
+
+const generateRandomID = () => {
+  const max = 1000000
+  return Math.floor(Math.random() * max)
+}
+
+const generateUniqueID = () => {
+  let tempID = generateRandomID()
+  while (isExistingID(tempID)) {
+    tempID = generateRandomID()
+  }
+  return tempID
+}
+
+const isExistingID = (id) => {
+  const idList = persons.map(person => person.id)
+  return idList.includes(id)
+}
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if (!body.name) {
+    return response.status(404).json({
+      error: "content missing"
+    })
+  }
+
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateUniqueID()
+  }
+
+  persons = persons.concat(newPerson)
+  response.json(newPerson)
 })
 
 const PORT = 3001
